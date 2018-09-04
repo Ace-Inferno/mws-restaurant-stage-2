65e780155ffe0cc102c8bc918fb9240d5812f3cf
Ace-Inferno <mathewhace@gmail.com>
2018-09-04 06:36:48 -0700

    combine stage 1 and 2

---
+++ js/idb-data.js
@@ -0,0 +1,32 @@
+let restaurantData;
+
+function handleSuccess () {
+  const json = JSON.parse(this.responseText);
+  caches.open('offline').then(function(cache){cache.add(`${DBHelper.DATABASE_URL}`)});
+  restaurantData = json;
+  console.log(restaurantData);
+}
+function handleError () {
+  console.log( 'An error occurred \uD83D\uDE1E' );
+}
+function getJSON(){
+  const database = new XMLHttpRequest();
+  database.open('GET', DBHelper.DATABASE_URL);
+  database.onload = handleSuccess;
+  database.onerror = handleError;
+  database.send();
+}
+function createIDB(){
+  const dbName = "Restaurant_Database";
+  var request = indexedDB.open(dbName, 1);
+  request.onupgradeneeded = function (event) {
+      var db = event.target.result;
+      var objStore = db.createObjectStore("Restaurant_Data", { autoIncrement : true });
+      restaurantData.forEach(function(restaurant) {
+          objStore.add(restaurant);
+      });
+  };
+}
+
+getJSON();
+createIDB();
